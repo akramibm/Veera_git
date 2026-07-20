8f96af39f4578f4bc206d8b975bfcd3bedca7446
akramibm <akramafridi02@gmail.com>
2026-07-20 23:16:19 +0530

    add python

---
+++ app.py
@@ -0,0 +1,9 @@
+def main():
+    print("Hello from Git and Python!")
+    
+    # Simple logic to test changes
+    user_name = "akramibm"
+    print(f"Welcome back, {user_name}!")
+
+if __name__ == "__main__":
+    main()
